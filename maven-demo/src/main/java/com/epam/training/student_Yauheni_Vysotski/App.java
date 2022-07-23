ddc0e16bfbd07aec5c6db40badc26b49f50193fb
Yauheni Vysotski <yauhenivysotski@gmail.com>
2022-07-23 14:13:33 +0200

    maven

---
+++ maven-demo/src/main/java/com/epam/training/student_Yauheni_Vysotski/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_Yauheni_Vysotski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
